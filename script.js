14efda51884d670cf53cdf611b99a447874a7237
FleetAdmiralJakob <jakob.roessner@outlook.de>
2022-10-06 21:45:15 +0200

    First Function

--- script.js
+++ script.js
@@ -6,7 +6,23 @@ window.addEventListener('load', function (){
     canvas.height = 500;
     
     class InputHandler {
-        
+        constructor(game) {
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                if ((   (e.key === 'ArrowUp') ||
+                        (e.key === 'ArrowDown')
+                ) && this.game.keys.indexOf(e.key) === -1){
+                    this.game.keys.push(e.key);
+                }
+                console.log(e.key);
+            });
+            window.addEventListener('keyup', e => {
+                if (this.game.keys.indexOf(e.key) > -1){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+                console.log(this.game.keys);
+            })
+        }
     }
     class Projectile {
         
@@ -15,7 +31,22 @@ window.addEventListener('load', function (){
         
     }
     class Player {
-        
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update(){
+            if (this.game.keys.includes('ArrowUp')) this.speedY = -1;
+            else if (this.game.keys.includes('ArrowDown')) this.speedY = 1;
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     class Enemy {
         
@@ -30,6 +61,28 @@ window.addEventListener('load', function (){
         
     }
     class Game {
-
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
+        }
+        update(){
+            this.player.update();
+        }
+        draw(context){
+            this.player.draw(context);
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height);
+    // animation loop
+    function animate(){
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 })
