5ecff247e981bba822fdad574bb714488afd812f
FleetAdmiralJakob <jakob.roessner@outlook.de>
2022-10-06 17:15:21 +0200

    Basic Setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Steampunk-Game
+
+✏️ Based on a game by Frank's Laboratory. **https://www.youtube.com/c/Frankslaboratory**
+
+🚀 Owner: FleetAdmiralJakob **https://github.com/FleetAdmiralJakob**
+
+📜 License: MIT
---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', function (){
+    //canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+    
+    class InputHandler {
+        
+    }
+    class Projectile {
+        
+    }
+    class Particle {
+        
+    }
+    class Player {
+        
+    }
+    class Enemy {
+        
+    }
+    class Layer {
+        
+    }
+    class Background {
+        
+    }
+    class UI {
+        
+    }
+    class Game {
+
+    }
+})
