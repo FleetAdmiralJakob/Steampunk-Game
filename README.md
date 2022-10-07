6b42c1ca7b4f9c8051a0e1782f190efbe4cb3a6c
FleetAdmiralJakob <jakob.roessner@outlook.de>
2022-10-07 21:16:34 +0200

    Some Improvements
    - Added UI
    - Added Enemies
    - Updated README.md
    - Added collision detection between rectangles
    - Added lives/score system
    - Added a win-and-lose system
    - Added game time

--- README.md
+++ README.md
@@ -11,3 +11,20 @@
 - 💞️ At the moment I'm also looking for someone who wants to develop this game with me.
 
 - 📫 How to reach me: jakob.roessner@outlook.de
+
+## Contributors ✨
+
+Thanks goes to these wonderful people who have improved the code and documentation to help this project grow. ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<table>
+  <tbody>
+    <tr>
+      <td align="center"><a href="https://github.com/FleetAdmiralJakob"><img src="https://avatars.githubusercontent.com/u/77451351?s=400&u=b59205206eba2ae0746903b0abb0f39fa56ccf2c&v=4" width="100px;" alt="Jakob Felix Eugen Rössner"/><br /><sub><b>Jakob Felix Eugen Rössner</b></sub></a><br /><a href="" title="Content">🖋</a><a href="" title="Code">💻</a><a href="https://github.com/up-for-grabs/up-for-grabs.net/commits?author=gewarren" title="Documentation">📖</a></td>
+    </tr>
+  </tbody>
+  <tfoot>
+
+  </tfoot>
+</table>
+
+Contributions of any kind are welcome!
--- script.js
+++ script.js
@@ -90,7 +90,34 @@ window.addEventListener('load', function (){
     }
 
     class Enemy {
-        
+        constructor(game) {
+            this.game = game;
+            this.x = this.game.width;
+            this.speedX = Math.random() * -1.5 - 0.5;
+            this.markedForDeletion = false;
+            this.lives = 5;
+            this.score = this.lives;
+        }
+        update(){
+            this.x += this.speedX;
+            if (this.x + this.width < 0) this.markedForDeletion = true;
+        }
+        draw(context){
+            context.fillStyle = 'red';
+            context.fillRect(this.x, this.y, this.width, this.height);
+            context.fillStyle = 'black';
+            context.font = '20px Helvetica';
+            context.fillText(this.lives, this.x, this.y);
+        }
+    }
+
+    class Angler1 extends Enemy {
+        constructor(game) {
+            super(game);
+            this.width = 228 * 0.2;
+            this.height = 196 * 0.2;
+            this.y = Math.random() * (this.game.height * 0.9 - this.height)
+        }
     }
 
     class Layer {
@@ -102,33 +129,131 @@ window.addEventListener('load', function (){
     }
 
     class UI {
-        
+        constructor(game) {
+            this.game = game;
+            this.fontSize = 25;
+            this.fontFamily = 'Helvetica';
+            this.color = 'white';
+        }
+        draw(context){
+            context.save();
+            context.fillStyle = this.color;
+            context.shadowOffsetX = 2;
+            context.shadowOffsetY = 2;
+            context.shadowColor = 'black';
+            context.font = this.fontSize + 'px ' + this.fontFamily;
+            // Score system
+            context.fillText('Score: ' + this.game.score, 20, 40);
+            // This part of the codes makes that you can see how much ammo left
+            for (let i = 0; i < this.game.ammo; i++){
+                context.fillRect(20 + 5 * i, 50, 3, 20);
+            }
+            // Game over messages
+            if (this.game.gameOver){
+                context.textAlign = 'center';
+                let message1;
+                let message2;
+                if (this.game.score > this.game.winningScore){
+                    message1 = 'You Win!';
+                    message2 = 'Well done';
+                } else {
+                    message1 = 'You lose';
+                    message2 = 'Try again next time!'
+                }
+                context.font ='50px ' + this.fontFamily;
+                context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 40);
+                context.font ='25px ' + this.fontFamily;
+                context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5 + 40);
+            }
+            context.restore();
+        }
     }
 
     class Game {
         constructor(width, height){
             this.width = width;
             this.height = height;
+            
             this.player = new Player(this);
             this.input = new InputHandler(this);
+            this.ui = new UI(this);
+            
             this.keys = [];
+            
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
+            
             this.ammo = 20;
+            this.maxAmmo = 50;
+            this.ammoTimer = 0;
+            this.ammoInterval =500;
+            
+            this.gameOver = false;
+            this.score = 0;
+            this.winningScore = 50;
         }
-        update(){
+        update(deltaTime){
             this.player.update();
+            if (this.ammoTimer > this.ammoInterval){
+                if (this.ammo < this.maxAmmo) this.ammo++;
+                this.ammoTimer = 0;
+            } else {
+                this.ammoTimer += deltaTime;
+            }
+            this.enemies.forEach(enemy => {
+                enemy.update();
+                if (this.checkCollision(this.player, enemy)){
+                    enemy.markedForDeletion = true;
+                }
+                this.player.projectiles.forEach(projectile => {
+                    if (this.checkCollision(projectile, enemy)){
+                        enemy.lives--;
+                        projectile.markedForDeletion = true;
+                        if (enemy.lives <= 0){
+                            enemy.markedForDeletion = true;
+                            this.score += enemy.score;
+                            if (this.score > this.winningScore) this.gameOver = true;
+                        }
+                    }
+                })
+            });
+            this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
+            if (this.enemyTimer > this.enemyInterval && !this.gameOver){
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
         }
         draw(context){
             this.player.draw(context);
+            this.ui.draw(context);
+            this.enemies.forEach(enemy => {
+                enemy.draw(context);
+            });
+        }
+        addEnemy(){
+            this.enemies.push(new Angler1(this));
+        }
+        checkCollision(rect1, rect2){
+            return (    rect1.x < rect2.x + rect2.width &&
+                        rect1.x + rect1.width > rect2.x &&
+                        rect1.y < rect2.y + rect2.height &&
+                        rect1.height + rect1.y > rect2.y)
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
     // Animation loop
-    function animate(){
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0,0, canvas.width, canvas.height);
-        game.update();
+        game.update(deltaTime);
         game.draw(ctx);
         requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
 })
