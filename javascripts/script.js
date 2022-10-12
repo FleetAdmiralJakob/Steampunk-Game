ae0c6bd7386abedcc2428172656724cce18d6f89
FleetAdmiralJakob <jakob.roessner@outlook.de>
2022-10-12 17:29:48 +0200

    Bigger

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
   // canvas setup
   const canvas = document.getElementById('canvas1')
   const ctx = canvas.getContext('2d')
-  canvas.width = 1000
+  canvas.width = 1400
   canvas.height = 500
 
   class InputHandler {
