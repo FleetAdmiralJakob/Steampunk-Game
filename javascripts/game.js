25363cd1ab833cde10da80c030371e9b0e16376e
FleetAdmiralJakob <jakob.roessner@outlook.de>
2022-11-12 16:23:56 +0100

    .

--- javascripts/game.js
+++ javascripts/game.js
@@ -9,15 +9,16 @@ window.addEventListener('load', function () {
     constructor (game) {
       this.game = game
       window.addEventListener('keydown', e => {
-        if (((e.key === 'ArrowUp') ||
-                        (e.key === 'ArrowDown') ||
+        if ((
+            (e.key === 'ArrowUp') ||
+            (e.key === 'ArrowDown') ||
             (e.key === 'w') ||
             (e.key === 's')
         ) && this.game.keys.indexOf(e.key) === -1) {
           this.game.keys.push(e.key)
         } else if (e.key === ' ') {
           this.game.player.shootTop()
-        } else if (e.key === 'd') {
+        } else if (e.key === 'e') {
           this.game.debug = !this.game.debug
         }
       })
